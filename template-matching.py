9bc8c327f217adcc347862f7c0809c57feac7e1c
Chris Gala <cgala@imri.com>
2017-05-26 20:33:30 -0700

    changed name of repo

--- template-matching.py
+++ template-matching.py
@@ -17,21 +17,21 @@ image = cv2.imread('./data/train/apple.png')
 image = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
 imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-templates.append(cv2.imread('./data/templates/apple.png'))
-templates.append(cv2.imread('./data/templates/arrow.png'))
-templates.append(cv2.imread('./data/templates/bed.png'))
-templates.append(cv2.imread('./data/templates/beef.png'))
-templates.append(cv2.imread('./data/templates/bowl.png'))
-templates.append(cv2.imread('./data/templates/bread.png'))
-templates.append(cv2.imread('./data/templates/book.png'))
-templates.append(cv2.imread('./data/templates/bone.png'))
-templates.append(cv2.imread('./data/templates/fish.png'))
-templates.append(cv2.imread('./data/templates/melon.png'))
-templates.append(cv2.imread('./data/templates/potion.png'))
-templates.append(cv2.imread('./data/templates/wooden_sword.png'))
+templates.append(('apple', cv2.imread('./data/templates/apple.png')))
+templates.append(('arrow', cv2.imread('./data/templates/arrow.png')))
+templates.append(('bed', cv2.imread('./data/templates/bed.png')))
+templates.append(('beef', cv2.imread('./data/templates/beef.png')))
+templates.append(('bowl', cv2.imread('./data/templates/bowl.png')))
+templates.append(('bread', cv2.imread('./data/templates/bread.png')))
+templates.append(('book', cv2.imread('./data/templates/book.png')))
+templates.append(('bone', cv2.imread('./data/templates/bone.png')))
+templates.append(('fish', cv2.imread('./data/templates/fish.png')))
+templates.append(('melon', cv2.imread('./data/templates/melon.png')))
+templates.append(('potion', cv2.imread('./data/templates/potion.png')))
+templates.append(('wooden_sword', cv2.imread('./data/templates/wooden_sword.png')))
 
 for t in range(len(templates)):
-    template = templates[t]
+    template = templates[t][1]
 
     # Resize template
     template = cv2.resize(template, (0,0), fx=0.5, fy=0.5)
@@ -44,6 +44,7 @@ for t in range(len(templates)):
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
     results[t] = {}
+    results[t]['name'] = templates[t][0]
     results[t]['min_val'] = min_val
     results[t]['max_val'] = max_val
     results[t]['min_loc'] = min_loc
@@ -55,7 +56,7 @@ for k, v in results.items():
         max_accuracy = results[k]['max_val']
         max_accuracy_idx = k
 
-print max_accuracy, max_accuracy_idx
+print max_accuracy, results[max_accuracy_idx]['name']
 
 top_left = results[max_accuracy_idx]['max_loc']
 h, w = results[max_accuracy_idx]['templateGray'].shape
@@ -67,4 +68,4 @@ cv2.imshow("Result", image)
 
 cv2.moveWindow("Result", 150, 50);
 
-cv2.waitKey(5000)
+cv2.waitKey(3000)
